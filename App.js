177aff1e169ec570ac1aa748ec30ded4a6fe2909
ffazal <faizanahmedfazal@gmail.com>
2020-10-08 04:26:36 -0400

    Added login button and Welcome Screen

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  StatusBar,
-  View,
-  SafeAreaView,
-  Button,
-  Alert,
-  Platform,
-} from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log("Tapped")} />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    //justifyContent: "center",
-    //alignItems: "center",
-  },
-});
---
+++ app/components/AppText.js
@@ -0,0 +1,16 @@
+// Encapsulating Styles - To keep same text styles acorss all platforms such as iOS, android etc
+import React from "react";
+import { Text, StyleSheet, Platform } from "react-native";
+
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
+
+export default AppText;
