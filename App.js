4129c3030a18473dd460f987dabec23e3dd38bd1
ffazal <faizanahmedfazal@gmail.com>
2020-11-14 05:09:41 -0500

    Using Request method, implemented Permissions API to access Camera Permission

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
 
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+//useEffect gets executed everytime our component gets rendered but since we only wanna run it the first time, useEffect hook has this additional optional parameter that we can set as empty list. Therefore, now this will only run once
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => { // Reqest method internally uses the Permission API
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync(); // Async method so we have to await it to get the result
+    if (!granted) alert("You need to enable permission to access the library");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return <Screen></Screen>;
 }
